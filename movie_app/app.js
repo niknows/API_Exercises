b21c879bf69a8162b5ae8b84c3c51a0c8e9c86f5
niknows <33270711+niknows@users.noreply.github.com>
2018-07-06 18:39:13 -0300

    Create app.js

---
+++ movie_app/app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+
+app.use(express.static("public"));
+app.set("view engine","ejs");
+
+app.get("/",function(req,res){
+    res.render("search");
+});
+app.get("/results",function(req,res){
+   var url = "http://www.omdbapi.com/?s=" + req.query.search + "&apikey=thewdb";
+   
+   request(url,function(error,response,body){
+       if(!error && response.statusCode == 200){
+          var parsed = JSON.parse(body);
+          res.render("results",{parsed: parsed});
+       }
+   });
+});
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("Movie App has started...");
+});
